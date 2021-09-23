981b84513d6794ef07788963bd7779a4a4bac5ee
ViniiGarcia <viniciusgrlopes@hotmail.com>
2021-09-23 19:28:04 -0300

    Projeto de números aleatórios

---
+++ numero_aleatorio/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:numero_aleatorio/telaInicial.dart';
+
+void main(){
+  runApp(App());
+}
+
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TelaInicial(),
+    );
+  }
+}
+
