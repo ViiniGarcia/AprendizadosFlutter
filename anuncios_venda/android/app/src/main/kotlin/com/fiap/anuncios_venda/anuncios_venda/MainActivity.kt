7ccf825297e3a8d88549f3604c8a9775d231663e
ViniiGarcia <viniciusgrlopes@hotmail.com>
2021-10-13 19:17:25 -0300

    Projeto Anuncios Venda

---
+++ anuncios_venda/android/app/src/main/kotlin/com/fiap/anuncios_venda/anuncios_venda/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fiap.anuncios_venda.anuncios_venda
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ anuncios_venda/lib/TelaInicial.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class TelaInicial extends StatefulWidget {
+  const TelaInicial({Key? key}) : super(key: key);
+
+  @override
+  _TelaInicialState createState() => _TelaInicialState();
+}
+
+class _TelaInicialState extends State<TelaInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _corpo(),
+    );
+  }
+
+  _corpo(){
+    return Container(
+
+    );
+  }
+}
