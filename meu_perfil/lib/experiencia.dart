adf6db112be827812b77d5bb8ff0cacf46041bdb
ViniiGarcia <viniciusgrlopes@hotmail.com>
2021-10-18 15:38:47 -0300

    Projeto Meu perfil

---
+++ meu_perfil/lib/experiencia.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Experiencia extends StatelessWidget {
+  const Experiencia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
